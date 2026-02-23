e8b35024b005b4feff5fc000331b02f1ef52166f
Johnf0356 <116595620+Johnf0356@users.noreply.github.com>
2026-02-22 22:26:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,31 @@ def inicio():
 # Ruta productos
 @app.route("/productos")
 def productos():
-    return render_template("productos.html")
+    productos = [
+        {'nombre': 'Sandalia Playa', 'precio': 20},
+        {'nombre': 'Sandalia Casual', 'precio': 30},
+        {'nombre': 'Sandalia Deportiva', 'precio': 40}
+    ]
+    return render_template("productos.html", productos=productos)
+
+# Ruta Acerca de
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Ruta factura
+@app.route("/factura")
+def factura():
+    return render_template("factura.html")
+
+# Ruta clientes
+@app.route("/clientes")
+def clientes():
+    clientes = [
+        {'nombre': 'Juan Pérez', 'email': 'juan@correo.com'},
+        {'nombre': 'María López', 'email': 'maria@correo.com'}
+    ]
+    return render_template("clientes.html", clientes=clientes)
 
 if __name__ == "__main__":
     app.run(debug=True)
